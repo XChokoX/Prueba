030343c73101a9acbcc944f0c5256f6463a911e4
Pans02 <robertocarrasco@alumnos.upla.cl>
2023-07-26 14:20:23 -0600

    Pruebaa

---
+++ Pruebaa.py
@@ -0,0 +1,63 @@
+#Ejercicio 1
+"""pregunta = input("Ingrese su valor: ")
+pregunta = float(pregunta)
+
+sueldo = 2400
+
+if pregunta == 0.0:
+    operacion = sueldo * pregunta
+    print("Su nivel es: inaceptable")
+    print(f"Su sueldo es de {operacion}")
+
+elif pregunta == 0.4:
+    operacion = sueldo * pregunta
+    print("Su nivel es: aceptable")
+    print(f"Su sueldo es de {operacion}")
+
+else:
+    operacion = sueldo * pregunta
+    print("Su nivel es: meritorio")
+    print(f"Su sueldo es de {operacion}")"""
+    
+#Ejercicio 2    
+"""def area_circulo(x):
+    
+    a = x**2
+    operacion = 3.14 * a
+
+    
+    print(f"Area: {operacion}")
+   
+   
+    return operacion
+
+def volumen(k,t):
+    
+    ñ = k * t
+    print(f"Volumen: {ñ}")
+    
+    return
+
+"""radio = input("Ingrese el valor radio del circulo: ")
+radio = float(radio)
+
+area_circulo(radio)"""
+
+altura = input("Ingrese la altura de su cilindo: ")
+altura = float(altura)
+
+radioc = input("Ingrese el valor radio del circulo: ")
+radioc = float(radioc)
+
+s = area_circulo(radioc)
+
+volumen(s,altura)"""
+
+
+#Ejercicio 3
+"""nacimento = input("Ingrese su fecha de nacimiento dd/mm/aaaa: ")
+
+
+print(f"Dia:{nacimento[0:2]}")
+print(f"Mes:{nacimento[3:5]}")
+print(f"Mes:{nacimento[6:10]}")"""
