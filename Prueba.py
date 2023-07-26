7596044d3ebd24a9ffcc400b6ab9809b1946a98c
Pans02 <robertocarrasco@alumnos.upla.cl>
2023-07-26 14:12:48 -0600

    Prueba

---
+++ Prueba.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 20:51:26 2023
+
+@author: Lab-105_Pc20
+"""
+
+pregunta = input("Ingrese su valor: ")
+pregunta = float(pregunta)
+
+sueldo = 2400
+
+operacion = sueldo * pregunta
+
+    if pregunta == 0.4:
+        print("Hola")
